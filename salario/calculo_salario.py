d119f7ca165ccbc2600f8ba1dd89b2bd2ea76bb7
LukeidDev <lukeid001@gmail.com>
2023-07-17 09:47:00 -0400

    conversao de salario

--- salario/calculo_salario.py
+++ salario/calculo_salario.py
@@ -1,11 +1,15 @@
 from salario.dig_salario import dig_salario, bonus_salario, desconto_salario
 from calculadora.operadores import soma, subtracao
+from salario.conversor import real_for_dolar, real_for_euro, real_for_yuan
 
 def calcular_salario():
     
     print("Selecione uma operação")
     print("1 - calcular salario total com bonificação")
     print("2 - calcular salario total com desconto")
+    print("3 - conversor de real para dolar")
+    print("4 - conversor de real para euro")
+    print("5 - conversor de real para yuan")
 
     select = int(input("Digite o valor selecionado: "))
     numero1 = dig_salario()
@@ -23,9 +27,23 @@ def calcular_salario():
 
         result = subtracao(numero1, numero2)
         print("O salário total ao final será de : %.2f" % result)
+
+    elif(select == 3):
+        result = real_for_dolar(numero1)
+        print("O salário total ao final será de : %.2f" % result)
+
+    elif(select == 4):
+        result = real_for_euro(numero1)
+        print("O salário total ao final será de : %.2f" % result)
+    elif(select == 5):
+        result = real_for_yuan(numero1)
+        print("O salário total ao final será de : %.2f" % result)
+
     
     else:
-        print("valor de operação incorreto")
+        result = dig_salario
+    
+
 
             
 
