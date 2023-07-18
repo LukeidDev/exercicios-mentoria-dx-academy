d384c3b3558bc20bcf2a98efb81be1b5684322ad
LukeidDev <luccasdaniel92@gmail.com>
2023-07-18 08:48:06 -0400

    manutenção

--- escolha.py
+++ escolha.py
@@ -1,5 +1,7 @@
-
 import time
+from imc.calculo_imc import calcular_imc
+from salario.calculo_salario import calcular_salario
+from calculadora.funcoes_calculadora import operacoes
 
 
 def escolha():
@@ -10,6 +12,18 @@ def escolha():
 
     escolha = int(input("Digite o valor escolhido: "))
     print("----------------------------------------------")
+
+    if(escolha == 1):
+        calcular_imc()
+        retornar()
+    elif(escolha == 2):
+        calcular_salario()
+        retornar()
+    elif(escolha == 3):
+        operacoes()
+        retornar()
+    else:
+        escolha()
 
     return escolha
 
